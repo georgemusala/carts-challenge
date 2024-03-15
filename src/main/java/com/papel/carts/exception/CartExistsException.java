1ca8373c30a88e117e58c20b226d82665e9fd41e
George Musala <george.musala@ext.mercedes-benz.io>
2024-03-15 11:22:38 +0200

    Remove redundant response status from custom exceptions

--- src/main/java/com/papel/carts/exception/CartExistsException.java
+++ src/main/java/com/papel/carts/exception/CartExistsException.java
@@ -1,9 +1,5 @@
 package com.papel.carts.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.CONFLICT)
 public class CartExistsException extends RuntimeException{
 
     public CartExistsException(String message) {
--- src/main/java/com/papel/carts/exception/CartNotFoundException.java
+++ src/main/java/com/papel/carts/exception/CartNotFoundException.java
@@ -1,9 +1,5 @@
 package com.papel.carts.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CartNotFoundException extends RuntimeException {
     public CartNotFoundException(String message) {
         super(message);
